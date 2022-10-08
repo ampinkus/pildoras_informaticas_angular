79fc57587b69ebed532566fc4d7d0f00ff9236b3
ampinkus <ampinkus@gmail.com>
2022-10-08 16:43:34 -0300

    Eventos

--- miprimeraApp/src/app/empleado/empleado.component.ts
+++ miprimeraApp/src/app/empleado/empleado.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //creo attributos del empleado
+  nombre = 'Fredi';
+  apellido = 'Pinkus';
+  edad = 60;
+  // empresa = '3Mar'
+
+  /* // creo un método que retorna el nombre de la empresa
+  getEmpresa() {
+    return this.empresa
+  }
+ */
+
+  llamaEmpresa(dato:String){ // lo unico que hace es tomar un valor de cadena de caracteres
   }
 
 }
