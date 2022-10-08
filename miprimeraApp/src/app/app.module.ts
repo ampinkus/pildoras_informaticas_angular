49341764ccbbb21378a5437acfd3e54cb43927ef
ampinkus <ampinkus@gmail.com>
2022-10-08 14:34:10 -0300

    Incluir empleados

--- miprimeraApp/src/app/app.module.ts
+++ miprimeraApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // debo importar el método AppComponent para declarar que es el método que da arranque a la página WEB en bootstrap:
 import { AppComponent } from './app.component';
-import { EmpleadosComponent } from './empleados/empleados.component'; 
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component'; 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadosComponent
+    EmpleadosComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule
