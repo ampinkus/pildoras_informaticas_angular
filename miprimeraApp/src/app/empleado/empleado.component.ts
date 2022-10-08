0b9e5f1841c29b9fe5b05efd084034e40dd08659
ampinkus <ampinkus@gmail.com>
2022-10-08 18:06:34 -0300

    09 Event Binding

--- miprimeraApp/src/app/empleado/empleado.component.ts
+++ miprimeraApp/src/app/empleado/empleado.component.ts
@@ -6,23 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
+  public razonSocial!:String;  // creo una varible RazonSocial para guardar el nombre de la empresa
   constructor() { }
 
   ngOnInit(): void {
   }
   //creo attributos del empleado
   nombre = 'Fredi';
-  apellido = 'Pinkus';
+  // apellido = 'Pinkus';
   edad = 60;
-  // empresa = '3Mar'
 
-  /* // creo un método que retorna el nombre de la empresa
-  getEmpresa() {
-    return this.empresa
+  habilitacionCuadro = true; // si es verdadero el cuadro de texto del apellido se encuentra deshabilitado
+
+  usuarioRegistrado = false;
+
+  getRegistroUsuario(){
+    this.usuarioRegistrado = !this.usuarioRegistrado; // cuando la llamo invierte el valor de usuarioRegistrado
   }
- */
+
 // lo unico que hace es que este disponible el valor de la cadena empresa.value.  Tiene que ser String o any
   llamaEmpresa(dato:any){ 
+    this.razonSocial =  dato; //guardo en razonSocial el nombre ingresado de la empresa 
   }
 
 }
