74307161cf567f9631df6ac1d374d626d0d508f3
ampinkus <ampinkus@gmail.com>
2022-11-16 15:28:19 -0300

    correcciones hasta el video 10

--- miprimeraApp/src/app/empleado/empleado.component.ts
+++ miprimeraApp/src/app/empleado/empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-empleado',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadoComponent implements OnInit {
   public razonSocial!: String;  // creo una varible RazonSocial para guardar el nombre de la empresa
-  //creo attributos del empleado
+  //creo atributos del empleado
   public nombre: String = '';
   // apellido = 'Pinkus';
   edad: Number = 0;
@@ -15,8 +16,9 @@ export class EmpleadoComponent implements OnInit {
   habilitacionCuadro = false; // si es verdadero el cuadro de texto del apellido se encuentra deshabilitado
   usuarioRegistrado = true; // si está en false no aparece el tick del campo, como se invoca luego la función getRegistroUsuario va a pasar a false
   textoDeRegistro = 'No hay nadie registrado'
-   // Creo una propiedad para controlar la posibilidad de habilitar o deshabilitar el ingreso del correo electrónico
-   habilitacionEmail:boolean = true; // inicial esta deshabilitado
+  // Creo una propiedad para controlar la posibilidad de habilitar o deshabilitar el ingreso del correo electrónico
+  habilitacionEmail: boolean = true; // inicial esta deshabilitado
+
 
   constructor() { }
 
@@ -41,11 +43,23 @@ export class EmpleadoComponent implements OnInit {
     this.razonSocial = dato; //guardo en razonSocial el nombre ingresado de la empresa 
   }
 
-  setUsuarioRegistrado() { // presento un alerta cuando hago click en el cuadro
-    alert('El usuario se acaba de registrar');
-    this.habilitacionEmail = false ; // habilito el campo para ingresar el Email
-  }
+  // presento un alerta cuando hago click en el check box
+  // cuando uso radio button capturo el evento que se genera
+  setUsuarioRegistrado(event: Event) {
+    // alert('El usuario se acaba de registrar');
+    // alert(event.target);  //quiero mostrar el valor del evento en este caso es [object HTMLInputElement]
+    this.textoDeRegistro = "El usuario se acaba de registrar";
+    this.habilitacionEmail = false; // habilito el campo para ingresar el Email
 
+    if ((<HTMLInputElement>event.target).value == "si") { // todo esto, casting incluido, para obtener el valor de value (si o no)
+      this.textoDeRegistro = "El usuario se acaba de registrar";
+    }
+    else{
+      this.textoDeRegistro = "No hay nadie registrado";
+      this.habilitacionEmail = true; // deshabilito el campo para ingresar el Email
+    }
+
+  }
 
 
 }
