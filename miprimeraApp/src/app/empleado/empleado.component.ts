8b3bc0c69d34107f32c8dde02fe57eede13784ae
ampinkus <ampinkus@gmail.com>
2022-11-17 14:50:26 -0300

    Completado two way binding

--- miprimeraApp/src/app/empleado/empleado.component.ts
+++ miprimeraApp/src/app/empleado/empleado.component.ts
@@ -4,23 +4,25 @@ import { EmailValidator } from '@angular/forms';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.css']
+  styleUrls: ['./empleado.component.css'],
 })
 export class EmpleadoComponent implements OnInit {
-  public razonSocial!: String;  // creo una varible RazonSocial para guardar el nombre de la empresa
+  public razonSocial!: String; // creo una varible RazonSocial para guardar el nombre de la empresa
   //creo atributos del empleado
   public nombre: String = '';
   // apellido = 'Pinkus';
   edad: Number = 0;
-  numeroLetras = 0;
+  // si pongo un nobre inicial en la empresa el mismo solo aparece en el campo "Empresa del empleado" => one way binding
+  // si fuese two way binding debería aparecer también en el campo "introduce el nombre de la empresa"  
+  empresa = 'Google'; 
+  numeroLetras: number = 0;
   habilitacionCuadro = false; // si es verdadero el cuadro de texto del apellido se encuentra deshabilitado
   usuarioRegistrado = true; // si está en false no aparece el tick del campo, como se invoca luego la función getRegistroUsuario va a pasar a false
-  textoDeRegistro = 'No hay nadie registrado'
+  textoDeRegistro = 'No hay nadie registrado';
   // Creo una propiedad para controlar la posibilidad de habilitar o deshabilitar el ingreso del correo electrónico
   habilitacionEmail: boolean = true; // inicial esta deshabilitado
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     //creo atributos del empleado
@@ -40,26 +42,28 @@ export class EmpleadoComponent implements OnInit {
 
   // lo unico que hace es que este disponible el valor de la cadena empresa.value.  Tiene que ser String o any
   llamaEmpresa(dato: any) {
-    this.razonSocial = dato; //guardo en razonSocial el nombre ingresado de la empresa 
+    this.razonSocial = dato; //guardo en razonSocial el nombre ingresado de la empresa
   }
 
-  // presento un alerta cuando hago click en el check box
+  // presento un alerta cuando hago click en el check box (actualmente no esta en uso)
   // cuando uso radio button capturo el evento que se genera
   setUsuarioRegistrado(event: Event) {
     // alert('El usuario se acaba de registrar');
     // alert(event.target);  //quiero mostrar el valor del evento en este caso es [object HTMLInputElement]
-    this.textoDeRegistro = "El usuario se acaba de registrar";
+    this.textoDeRegistro = 'El usuario se acaba de registrar';
     this.habilitacionEmail = false; // habilito el campo para ingresar el Email
 
-    if ((<HTMLInputElement>event.target).value == "si") { // todo esto, casting incluido, para obtener el valor de value (si o no)
-      this.textoDeRegistro = "El usuario se acaba de registrar";
-    }
-    else{
-      this.textoDeRegistro = "No hay nadie registrado";
+    if ((<HTMLInputElement>event.target).value == 'si') {
+      // todo esto, casting incluido, para obtener el valor de value (si o no)
+      this.textoDeRegistro = 'El usuario se acaba de registrar';
+    } else {
+      this.textoDeRegistro = 'No hay nadie registrado';
       this.habilitacionEmail = true; // deshabilito el campo para ingresar el Email
     }
-
   }
 
-
+  // tomo el valor del campo que llamo a esta función y lo envío a empresa
+  cambiaEmpresa(event: Event) {
+    this.empresa = (<HTMLInputElement>event.target).value;
+  }
 }
