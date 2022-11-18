356a379aaf896968b0d303145ec385a73c756b75
ampinkus <ampinkus@gmail.com>
2022-11-18 12:28:21 -0300

    Added funcion suma

--- MiniCalculadoraApp/src/app/app.component.ts
+++ MiniCalculadoraApp/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+  //creamos las clases para almacenar los valores numericos
+  numero1: number = 0; //los campos donde ingreso las variables
+  numero2: number = 0;
+  resultado: number = 0; // el campo resultado
+
+  //creo los métodos para hacer los cálculos
+  
+  sumar(): void {
+  this.resultado =  Number(this.numero1) + Number(this.numero2);
+  }
 }
