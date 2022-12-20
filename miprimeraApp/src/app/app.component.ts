097fd2cde6bf1014b2bc2074888bc34288c3425a
ampinkus <ampinkus@gmail.com>
2022-12-20 20:26:53 -0300

    Mas comentarios sobre el orden de inicio de una app Angular

--- miprimeraApp/src/app/app.component.ts
+++ miprimeraApp/src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 
-@Component({ // decorador que acompaña a la clase AppComponent
-  selector: 'app-root', 
-/* 
-   este selector hace que en index.html al encontar: 
+@Component({
+  // decorador que acompaña a la clase AppComponent
+  selector: 'app-root',
+  /* 
+   este selector: selector: 'app-root', hace que en index.html al encontrar: 
    <body>
       <app-root></app-root>
     </body>
    se navegue a 'app.component.html' junto con la página de estilo de 'app.component.css'
 */
-  templateUrl: './app.component.html',  
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-// Esta es la funcion que se va  ejecutar en:  bootstrap: [AppComponent]. 
-// O sea que la página WEB comienza con el método que está encerrado entre [] luego de bootstrap:  
-export class AppComponent {  
+// Esta es la función que se va  ejecutar en: bootstrap: [AppComponent].
+// O sea que la página WEB comienza con el método que está encerrado entre [] luego de bootstrap:
+export class AppComponent {
   title = 'miPrimeraApp';
-  saludo = 'Hola Angular' 
-} 
+  subtitle = 'muy sencilla';
+  saludo = 'Hola Angular';
+}
